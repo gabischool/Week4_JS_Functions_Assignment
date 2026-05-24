836a855b65f5d54824e3c1c9a598093f788aeaa9
Duraan Ali <16447314+duraanali@users.noreply.github.com>
2026-05-24 14:24:51 -0500

    finished work

--- challenges.js
+++ challenges.js
@@ -18,7 +18,11 @@ Output: "The late fee is $2.50."
 
 // ✍️ ✍️ ✍️ ✍️ Write the function here ✍️ ✍️ ✍️ ✍️
 
+function sayHi() {
+   console.log("Hi")
+}
 
+sayHi()
 
 // Extra Task:
 // - Convert the function into a function expression.
---
+++ steps.js
@@ -0,0 +1,8 @@
+// Step 1: Fork
+// Step 2: Clone the project (git clone URL)
+// Step 3: Open Visual studio code, open terminal within VS Code, create new branch (git checkout -b BRANCHNAME)
+// Step 4: Work on your assignment
+// Step 5: Send your work with these steps:
+        // 1. git add .
+        // 2. git commit -m "finished work"
+        // 3. git push -u origin duraantest
